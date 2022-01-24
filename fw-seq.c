1725e93a2236b5c9bab0ef129c48ccfc4d793b0f
kevinsena01 <keevinsenna2001@gmail.com>
2022-01-24 01:33:33 -0300

    Ignorando k-ésima linha/coluna

--- fw-seq.c
+++ fw-seq.c
@@ -32,7 +32,9 @@ void print_mat(int *mat_dist, int n){
 void fw(int *mat, int n){
 	for(int k = 0; k < n; k++){
 		for(int i = 0; i < n; i++){
+			if(i == k) continue;
 			for(int j = 0; j < n; j++){
+				if(j == k || i == j) continue;
 				if( mat[i * n + j] > mat[i * n + k] + mat[k * n + j] )
 					mat[i * n + j] = mat[i * n + k] + mat[k * n + j];
 			}	
@@ -42,10 +44,11 @@ void fw(int *mat, int n){
 }
 
 int main(int argc, char* argv[]){
-	int n;			     // Tamanho da matriz quadrada
+	int n;			     // Tamanho da matriz quadrada e um contador
 	int *mat_dist;		    //  Matriz distância advinda do grafo
 	FILE *arq; 		   //   Ponteiro para arquivo
 	char str[TAM_MAX_STRING]; //    String auxiliar para leitura da matriz
+
 
 	// Verificação inicial
 	if(argc < 2){
@@ -70,11 +73,10 @@ int main(int argc, char* argv[]){
 			mat_dist[i * n + j] = strcmp(str, "N") == 0? INFINITO : atoi(str);
 		}
 	}
-
-
+	
 	// Algoritmo de Floyd-Warshall
 	fw(mat_dist, n);
-
+	
 	// Printa resultado
 	print_mat(mat_dist, n);
 	
